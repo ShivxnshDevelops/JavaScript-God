70377cf9e26655d6499cdc1f9677762bfd071604
ShivxnshDevelops <shivxnshdevelops@gmail.com>
2024-11-23 17:34:48 +0530

    Initial Files

---
+++ Wtf Shivxnsh/index.js
@@ -0,0 +1,19 @@
+console.log("Day 1 Of Js Mastery For Shivxnsh")
+  const author = "Shivxnsh"
+console.log(author)
+
+let x = "KKKKKKKKKk"
+
+{
+  let x = "oh no"
+  console.log(x)
+}
+const y = 77
+console.log(x)
+const dumb = "there is someone who is adored by Shivxnsh"
+
+console.log(dumb)
+let nx = "i m so grateful to have someone like you ya know "
+console.log(nx)
+// it is very ezy to me but for idk 
+//not quite but it is enough for today
---
+++ Wtf Shivxnsh/wtf.js
@@ -0,0 +1,1 @@
+console.log("I am Goinng to be great coder thans for comig")
