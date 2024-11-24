6237bbaea49e701af3086b7b428165f7d237fb4e
ShivxnshDevelops <shivxnshdevelops@gmail.com>
2024-11-24 09:27:05 +0530

    Code Dump Of Early Stages Of Learning

---
+++ Wtf Shivxnsh/funcx.js
@@ -0,0 +1,9 @@
+console.log("I am Goinng to be great coder thans for comig")
+
+//This Is Js file Of Functions In JS
+
+function greet(name){
+    return 'Howdy, ${name}!';
+}
+
+console.log(greet("SHiv4nsh")) //Need Some Work 
---
+++ Wtf Shivxnsh/xd.js
@@ -0,0 +1,7 @@
+const orgname = "Artech Studios"
+const FoundingYear = 2024
+let Active = true
+let foundingant = 10000
+console.log("The Name Of The Org is" , orgname , "\nThe Founding Year Of Org Is" , FoundingYear ,"\n Startus Of The Org is Sctive -" ,
+Active ,"\n Funding By The Memebers -" , foundingant) 
+//
